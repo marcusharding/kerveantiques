f85e3dd841f6e81af4221b06eca3a63d2a49f00b
marcusharding <marcus.harding01@googlemail.com>
2020-07-24 22:22:18 +0100

    add favicon.ico

--- src/js/behaviours/loadingScreen.js
+++ src/js/behaviours/loadingScreen.js
@@ -3,16 +3,15 @@
 function loadingScreen(node) {
     const body = document.getElementById('body')
     const delayInMilliseconds = 2000
-    const container = node.childNodes
+    const container = document.getElementById('homeLoadingContainer')
     const siteHeader = document.getElementById('siteHeader')
     body.classList.add('fixed')
 
     window.onload = (event) => {
-    console.log('loaded')
     setTimeout(() => {
         body.classList.remove('fixed')
         body.classList.add('relative')
-        container[1].classList.add('isActive')
+        container.classList.add('isActive')
         siteHeader.classList.add('z-1000')
     }, delayInMilliseconds)
     }
